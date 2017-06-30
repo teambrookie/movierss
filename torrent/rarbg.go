d4e4d40b17df09f6d3ede20c3c76442b0bfef573
Fabien Foerster <fabienfoerster@gmail.com>
2017-06-30 20:36:45 +0200

    Filter audio quality

--- torrent/rarbg.go
+++ torrent/rarbg.go
@@ -1,14 +1,80 @@
 package torrent
 
-import torrentapi "github.com/qopher/go-torrentapi"
+import (
+	"strings"
 
-func goodEnoughTorrent(results torrentapi.TorrentResults) string {
-	for _, t := range results {
-		if t.Seeders > 0 || t.Leechers > 0 {
-			return t.Download
+	"log"
+
+	torrentapi "github.com/qopher/go-torrentapi"
+)
+
+func filterMovies(torrents torrentapi.TorrentResults) string {
+	var moviesextended torrentapi.TorrentResults
+	// Search for extended version
+	for _, t := range torrents {
+		var filename = strings.ToLower(t.Filename)
+		if strings.Contains(filename, "extended") {
+			moviesextended = append(moviesextended, t)
 		}
 	}
+	log.Println(torrents)
+	var results torrentapi.TorrentResults
+	results = filteraudioQuality("DTS-HD", moviesextended)
+	//log.Printf("For quality %s the number of result if %d", "DTS-HD", len(results))
+	if len(results) > 0 {
+		return results[0].Download
+	}
+	results = filteraudioQuality("DTS-HD.MA.7.1", moviesextended)
+	//log.Printf("For quality %s the number of result if %d", "DTS-HD.MA.7.1", len(results))
+	if len(results) > 0 {
+		return results[0].Download
+	}
+	results = filteraudioQuality("TrueHD.7.1Atmos", moviesextended)
+	//log.Printf("For quality %s the number of result if %d", "TrueHD.7.1Atmos", len(results))
+	if len(results) > 0 {
+		return results[0].Download
+	}
+	results = filteraudioQuality("DTS", moviesextended)
+	//log.Printf("For quality %s the number of result if %d", "DTS", len(results))
+	if len(results) > 0 {
+		return results[0].Download
+	}
+
+	results = filteraudioQuality("DTS-HD", torrents)
+	//log.Printf("For quality %s the number of result if %d", "DTS-HD", len(results))
+	if len(results) > 0 {
+		return results[0].Download
+	}
+	results = filteraudioQuality("DTS-HD.MA.7.1", torrents)
+	//log.Printf("For quality %s the number of result if %d", "DTS-HD.MA.7.1", len(results))
+	if len(results) > 0 {
+		return results[0].Download
+	}
+	results = filteraudioQuality("TrueHD.7.1Atmos", torrents)
+	//log.Printf("For quality %s the number of result if %d", "TrueHD.7.1Atmos", len(results))
+	if len(results) > 0 {
+		return results[0].Download
+	}
+	results = filteraudioQuality("DTS", torrents)
+	//log.Printf("For quality %s the number of result if %d", "DTS", len(results))
+	if len(results) > 0 {
+		return results[0].Download
+	}
+
 	return ""
+
+}
+
+func filteraudioQuality(quality string, torrents torrentapi.TorrentResults) torrentapi.TorrentResults {
+	var movies torrentapi.TorrentResults
+	for _, t := range torrents {
+		var filename = strings.ToLower(t.Download)
+		quality = strings.ToLower(quality)
+		if strings.Contains(filename, quality) {
+			movies = append(movies, t)
+		}
+	}
+	return movies
 }
 
 func Search(movieIMBDID, quality string) (string, error) {
@@ -17,6 +83,7 @@ func Search(movieIMBDID, quality string) (string, error) {
 		return "", err
 	}
 	api.Format("json_extended")
+	api.Category(44)
 	api.SearchImDB(movieIMBDID)
 	results, err := api.Search()
 	if err != nil {
@@ -26,5 +93,5 @@ func Search(movieIMBDID, quality string) (string, error) {
 	if len(results) == 0 {
 		return "", nil
 	}
-	return goodEnoughTorrent(results), nil
+	return filterMovies(results), nil
 }
